81cb12d8d9576cef09bb55108dca211e280697b3
chajae <60876477+chajeayoung@users.noreply.github.com>
2020-09-28 20:51:21 +0900

    2051

--- react_blog/chajae_blog/src/App.js
+++ react_blog/chajae_blog/src/App.js
@@ -7,19 +7,31 @@ import './App.css';
 function App() {
   let posts = '강남 고기 맛집';
 
-  let [글제목,글제목변경] = useState('남자 코트 추천');  //[a,b]
+  let [글제목,글제목변경] = useState(['나','하','가']);  //[a,b]
   let [글제목2,글제목변경2] = 
   useState(['차','재','영']);  //[a,b]
-  let [좋아요, 좋아요변경] = useState(0);
-  
-  
+  let [좋아요, 좋아요변경] = useState(0); 
+
+  function 제목바꾸기(){
+    var newArray = [...글제목]; // deep copy 
+    newArray = newArray.sort();
+    글제목변경(newArray);
+  }
   return (
     <div className="App">
         <div className="black-nav">
-                <div>개발 Blog</div>                
+                <div>{글제목}</div>                
         </div>
         <div className="list">
-  <h3>{ 글제목2[0] } <span onClick={ ()=>{좋아요변경(좋아요+1)}}>👍</span>{좋아요}</h3>
+
+          <button onClick={제목바꾸기}>버튼</button>
+  <h3>{ 글제목2[0] }
+
+   <span onClick={ ()=>{좋아요변경(좋아요+1)}}>👍</span>{좋아요}
+
+
+   </h3>
+
             <p>차랏 차차차~파게티</p>
             <hr/>
         </div>
